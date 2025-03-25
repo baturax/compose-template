e938cf5ca285a4e10593e79011c7a05ee9a0911a
Bai <bai@baturax.com>
2025-03-25 12:19:33 +0300

    Initial commit

---
+++ app/src/main/java/bai/bai/MainActivity.kt
@@ -0,0 +1,18 @@
+package bai.bai
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import bai.bai.ui.theme.BaiTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            BaiTheme {
+            }
+        }
+    }
+}
